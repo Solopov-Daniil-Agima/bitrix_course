f9988e25185aebedd0d3e39cc0e6774214dea096
Daniil <d.solopov@agima.ru>
2023-04-07 04:43:13 +0300

    пример 2, все работает как надо

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,23 +1,53 @@
 <?php
+function dump($elem){
+    echo "<pre>";
+    print_r($elem);
+    echo "</pre>";
+}
 //if(file_exists($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/include/event_handler.php")){
 //    require_once ($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/include/event_handler.php");
 //}
-AddEventHandler("iblock", "OnBeforeIBlockElementUpdate", Array("CIBlockHandler", "OnBeforeIBlockElementUpdateHandler"));
+//AddEventHandler("iblock", "OnBeforeIBlockElementUpdate", Array("CIBlockHandler", "OnBeforeIBlockElementUpdateHandler"));
+//
+//class CIBlockHandler
+//{
+//    // создаем обработчик события "OnBeforeIBlockElementUpdate"
+//    public static function OnBeforeIBlockElementUpdateHandler(&$arFields)
+//    {
+//        if($arFields['IBLOCK_ID'] == 2){
+//            $db_props = CIBlockElement::GetProperty($arFields['IBLOCK_ID'], $arFields['ID'], array("sort" => "asc"), Array("CODE"=>"PRICE"));
+//            if($ar_props = $db_props->Fetch()) {
+//                if(strlen($arFields['PROPERTY_VALUES'][$ar_props['ID']][$ar_props['PROPERTY_VALUE_ID']]['VALUE'] > 0)){
+//                    $arFields['PROPERTY_VALUES'][$ar_props['ID']][$ar_props['PROPERTY_VALUE_ID']]['VALUE'] =  preg_replace("/([а-яёa-z]+)/","", $arFields['PROPERTY_VALUES'][$ar_props['ID']][$ar_props['PROPERTY_VALUE_ID']]['VALUE']);
+//                }
+//            }
+//        }
+//
+//    }
+//}
 
-class CIBlockHandler
+AddEventHandler("main", "OnBeforeEventAdd", array("MyClass", "OnBeforeEventAddHandler"));
+class MyClass
 {
-    // создаем обработчик события "OnBeforeIBlockElementUpdate"
-    public static function OnBeforeIBlockElementUpdateHandler(&$arFields)
+    public static function OnBeforeEventAddHandler(&$event, &$lid, &$arFields)
     {
-        if($arFields['IBLOCK_ID'] == 2){
-            $db_props = CIBlockElement::GetProperty($arFields['IBLOCK_ID'], $arFields['ID'], array("sort" => "asc"), Array("CODE"=>"PRICE"));
-            if($ar_props = $db_props->Fetch()) {
-                if(strlen($arFields['PROPERTY_VALUES'][$ar_props['ID']][$ar_props['PROPERTY_VALUE_ID']]['VALUE'] > 0)){
-                    $arFields['PROPERTY_VALUES'][$ar_props['ID']][$ar_props['PROPERTY_VALUE_ID']]['VALUE'] =  preg_replace("/[^\d]+/","", $arFields['PROPERTY_VALUES'][$ar_props['ID']][$ar_props['PROPERTY_VALUE_ID']]['VALUE']);
-                }
+        if ($event == "FEEDBACK_FORM"){
+            if(CModule::IncludeModule("iblock")){
+                $el = new CIBlockElement;
+                $arLoadProductArray = [
+                    "IBLOCK_ID" => 5,
+                    "NAME" => $arFields['AUTHOR'],
+                    "DETAIL_TEXT" => $arFields['TEXT'],
+                    "DATE_ACTIVE_FROM" => ConvertTimeStamp(false, "FULL"),
+                ];
+                $el->Add($arLoadProductArray);
             }
         }
-
+//        dump($event);
+//        dump($lid);
+//        dump($arFields);
+//        die();
     }
 }
+
 ?>
