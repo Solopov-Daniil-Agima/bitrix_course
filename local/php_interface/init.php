8a8409e77f8303a6a4f571e59861b215381b2dc1
Daniil <d.solopov@agima.ru>
2023-04-07 21:22:50 +0300

    все исправил, все работает

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -7,24 +7,26 @@ function dump($elem){
 //if(file_exists($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/include/event_handler.php")){
 //    require_once ($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/include/event_handler.php");
 //}
-//AddEventHandler("iblock", "OnBeforeIBlockElementUpdate", Array("CIBlockHandler", "OnBeforeIBlockElementUpdateHandler"));
-//
-//class CIBlockHandler
-//{
-//    // создаем обработчик события "OnBeforeIBlockElementUpdate"
-//    public static function OnBeforeIBlockElementUpdateHandler(&$arFields)
-//    {
-//        if($arFields['IBLOCK_ID'] == 2){
-//            $db_props = CIBlockElement::GetProperty($arFields['IBLOCK_ID'], $arFields['ID'], array("sort" => "asc"), Array("CODE"=>"PRICE"));
-//            if($ar_props = $db_props->Fetch()) {
-//                if(strlen($arFields['PROPERTY_VALUES'][$ar_props['ID']][$ar_props['PROPERTY_VALUE_ID']]['VALUE'] > 0)){
-//                    $arFields['PROPERTY_VALUES'][$ar_props['ID']][$ar_props['PROPERTY_VALUE_ID']]['VALUE'] =  preg_replace("/([а-яёa-z]+)/","", $arFields['PROPERTY_VALUES'][$ar_props['ID']][$ar_props['PROPERTY_VALUE_ID']]['VALUE']);
-//                }
-//            }
-//        }
-//
-//    }
-//}
+AddEventHandler("iblock", "OnBeforeIBlockElementUpdate", Array("CIBlockHandler", "OnBeforeIBlockElementUpdateHandler"));
+
+class CIBlockHandler
+{
+    // создаем обработчик события "OnBeforeIBlockElementUpdate"
+    public static function OnBeforeIBlockElementUpdateHandler(&$arFields)
+    {
+
+        if($arFields['IBLOCK_ID'] == 2){
+
+            $db_props = CIBlockElement::GetProperty($arFields['IBLOCK_ID'], $arFields['ID'], array("sort" => "asc"), Array("CODE"=>"PRICE"));
+            if($ar_props = $db_props->Fetch()) {
+                if(strlen($arFields['PROPERTY_VALUES'][$ar_props['ID']][$ar_props['PROPERTY_VALUE_ID']]['VALUE']) > 0){
+                    $arFields['PROPERTY_VALUES'][$ar_props['ID']][$ar_props['PROPERTY_VALUE_ID']]['VALUE'] =  preg_replace("/[^\d]/","", $arFields['PROPERTY_VALUES'][$ar_props['ID']][$ar_props['PROPERTY_VALUE_ID']]['VALUE']);
+                }
+            }
+        }
+
+    }
+}
 
 //AddEventHandler("main", "OnBeforeEventAdd", array("MyClass", "OnBeforeEventAddHandler"));
 //class MyClass
@@ -46,20 +48,20 @@ function dump($elem){
 //    }
 //}
 
-AddEventHandler("main", "OnBeforeUserAdd", Array("MyClass", "OnBeforeUserAddHandler"));
-
-class MyClass
-{
-    // создаем обработчик события "OnBeforeUserAdd"
-    public static function OnBeforeUserAddHandler(&$arFields)
-    {
-        if($arFields["LAST_NAME"] == $arFields["NAME"])
-        {
-            global $APPLICATION;
-            $APPLICATION->throwException("Извините, но имя не может совпадать с фамилией.");
-            return false;
-        }
-    }
-}
+//AddEventHandler("main", "OnBeforeUserAdd", Array("MyClass", "OnBeforeUserAddHandler"));
+//
+//class MyClass
+//{
+//    // создаем обработчик события "OnBeforeUserAdd"
+//    public static function OnBeforeUserAddHandler(&$arFields)
+//    {
+//        if($arFields["LAST_NAME"] == $arFields["NAME"])
+//        {
+//            global $APPLICATION;
+//            $APPLICATION->throwException("Извините, но имя не может совпадать с фамилией.");
+//            return false;
+//        }
+//    }
+//}
 
 ?>
